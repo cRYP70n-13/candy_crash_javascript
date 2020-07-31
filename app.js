9fc16dfe0ed59edb85f63a90365d3ca4dc3d2210
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-07-31 20:27:11 +0100

    adding the drag function and event listeners

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		'green',
 		'blue'
 	]
+
 	// Create a board using JS
 	function createBoard() {
 		for (let i = 0; i < width * width; i++) {
@@ -25,4 +26,36 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	createBoard();
-})
+
+	// Drag the candies
+	squares.forEach(square => square.addEventListener('dragstart', dragstart));
+	squares.forEach(square => square.addEventListener('dragend', dragend));
+	squares.forEach(square => square.addEventListener('dragover', dragover));
+	squares.forEach(square => square.addEventListener('dragenter', dragenter));
+	squares.forEach(square => square.addEventListener('dragleave', dragleave));
+	squares.forEach(square => square.addEventListener('drop', drop));
+
+	function dragstart() {
+		console.log('this is dragstart');
+	}
+
+	function dragend() {
+		console.log('this is dragend');
+	}
+
+	function dragover() {
+		console.log('this is dragover');
+	}
+
+	function dragenter() {
+		console.log('this is draenter');
+	}
+
+	function dragleave() {
+		console.log('this is dragleave')
+	}
+
+	function drop() {
+		console.log('this is drop');
+	}
+});
