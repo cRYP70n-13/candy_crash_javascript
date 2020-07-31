4c67ca829ec672b4bc3a0fa787c9b0b913e005b5
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-07-31 21:29:45 +0100

    adding some new functionnalities to the grid and to the candies so we can control it like we have in the real gaem

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const grid = document.querySelector('.grid');
 	const width = 8;
 	const squares = [];
+	let score = 0;
 
 	const candyColors = [
 		'red',
@@ -90,4 +91,45 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	// check for matches
+	// check for row of three
+	function checkRowForThree() {
+		for (i = 0; i < 61; i++) {
+			let rowOfThree = [i, i+1, i+2]
+			let decidedColor = squares[i].style.backgroundColor
+			const isBlank = squares[i].style.backgroundColor === ''
+
+			if (rowOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+				score += 3
+				rowOfThree.forEach(index => {
+					squares[index].style.backgroundColor = '';
+				})
+			}
+		}
+	}
+	checkRowForThree();
+
+	// check for column of three
+	function checkColumnForThree() {
+		for (i = 0; i < 47; i++) {
+			let columnOfThree = [i, i + width, i + width * 2];
+			let decidedColor = squares[i].style.backgroundColor
+			const isBlank = squares[i].style.backgroundColor === ''
+
+			const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
+			if (notValid.includes(i)) continue
+
+			if (columnOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+				score += 3
+				columnOfThree.forEach(index => {
+					squares[index].style.backgroundColor = '';
+				})
+			}
+		}
+	}
+	checkColumnForThree();
+
+	window.setInterval(function (){
+		checkRowForThree();
+		checkColumnForThree();
+	}, 100);
 });
